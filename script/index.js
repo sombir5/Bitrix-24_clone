7862ed3248868dedb6e4f97cc61d2dee0f987f81
Rishav Kumar <rishavkumar121099@gmail.com>
2022-07-20 20:01:50 +0530

    update landing page

---
+++ script/index.js
@@ -0,0 +1,8 @@
+
+import navbar from "../component/navbar.js";
+
+console.log(navbar());
+document.getElementById("navbar").innerHTML = navbar();
+
+import footer from "../component/footer.js";
+document.querySelector(".footer").innerHTML= footer();
