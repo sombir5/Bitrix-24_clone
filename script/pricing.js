81bb748d0b3f0a73e06877ae3468022fbd154357
sombir5 <sombirverma5@gmail.com>
2022-07-21 20:03:55 +0530

    pricing-1

---
+++ script/pricing.js
@@ -0,0 +1,5 @@
+import navbar from "../component/navbar.js"
+document.getElementById('navbar').innerHTML=navbar();
+
+import footer from "../component/footer.js"
+document.querySelector('.footer').innerHTML=footer();
--- script/tools.js
+++ script/tools.js
@@ -5,7 +5,7 @@ import footer from "../component/footer.js"
 document.querySelector('.footer').innerHTML=footer();
 
 let element=document.querySelectorAll(".points")
-console.log(element)
+// console.log(element)
 element.forEach(e => {
     e.addEventListener('click',function(){
         changeColor(this)
@@ -17,11 +17,47 @@ function changeColor(element){
     element.style.backgroundColor ="white";
     element.style.color ="black";
 }
+ function clear(){
+            for(let i=0;i<element.length;i++){
+            let items= element[i];
+            items.style.backgroundColor ="#3baaf1";
+            items.style.color ="white";
+            // items.style.hover.backgroundColor="#ddf0fdd4"
+            }
+            }
 
-function clear(){
-for(let i=0;i<element.length;i++){
-let items= element[i];
-items.style.backgroundColor ="#3baaf1";
-items.style.color ="white";
-}
-}
+// element.forEach(e => {
+//     e.addEventListener('mouseover',function(){
+//         hover(this)
+//     });
+// });
+
+// function hover(element){
+//     console.log(element)
+//     clearHover();
+//     element.style.backgroundColor="#ddf0fdd4"
+//     }
+
+//     function clearHover(){  
+//         for(let i=0;i<element.length;i++){
+//         let items= element[i];
+//         items.style.backgroundColor ="#3baaf1";
+//         items.style.color ="white";
+//         // items.style.hover.backgroundColor="#ddf0fdd4"
+//         }
+//         }
+
+let signuppage=document.querySelectorAll('.free');
+
+
+signuppage.forEach(e => {
+    e.addEventListener('click',function(){
+        window.location.href="./signup.html"
+    });
+});
+// function changeColor(element){
+//     clear();
+//     console.log(element)
+//     element.style.backgroundColor ="white";
+//     element.style.color ="black";
+// }
