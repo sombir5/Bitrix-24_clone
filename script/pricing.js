470283b4d0638f2429133a091770d6529133cc73
Sombir <105915339+sombir5@users.noreply.github.com>
2022-07-21 20:31:14 +0530

    Revert "Fw19 0187 day3"

--- script/pricing.js
+++
@@ -1,5 +0,0 @@
-import navbar from "../component/navbar.js"
-document.getElementById('navbar').innerHTML=navbar();
-
-import footer from "../component/footer.js"
-document.querySelector('.footer').innerHTML=footer();
--- script/tools.js
+++ script/tools.js
@@ -1,13 +1,12 @@
-
-import navbar from "../component/navbar.js"
+import {navbar} from "../component/navbar.js";
 document.getElementById('navbar').innerHTML=navbar();
 
-import footer from "../component/footer.js"
+import footer from "../component/footer.js";
 document.querySelector('.footer').innerHTML=footer();
+
 
 let element=document.querySelectorAll(".points")
 console.log(element)
-
 element.forEach(e => {
     e.addEventListener('click',function(){
         changeColor(this)
@@ -20,48 +19,10 @@ function changeColor(element){
     element.style.color ="black";
 }
 
- function clear(){
-            for(let i=0;i<element.length;i++){
-            let items= element[i];
-            items.style.backgroundColor ="#3baaf1";
-            items.style.color ="white";
-            // items.style.hover.backgroundColor="#ddf0fdd4"
-            }
-            }
-
-// element.forEach(e => {
-//     e.addEventListener('mouseover',function(){
-//         hover(this)
-//     });
-// });
-
-// function hover(element){
-//     console.log(element)
-//     clearHover();
-//     element.style.backgroundColor="#ddf0fdd4"
-//     }
-
-//     function clearHover(){  
-//         for(let i=0;i<element.length;i++){
-//         let items= element[i];
-//         items.style.backgroundColor ="#3baaf1";
-//         items.style.color ="white";
-//         // items.style.hover.backgroundColor="#ddf0fdd4"
-//         }
-//         }
-
-let signuppage=document.querySelectorAll('.free');
-
-
-signuppage.forEach(e => {
-    e.addEventListener('click',function(){
-        window.location.href="./signup.html"
-    });
-});
-// function changeColor(element){
-//     clear();
-//     console.log(element)
-//     element.style.backgroundColor ="white";
-//     element.style.color ="black";
-// }
-
+function clear(){
+for(let i=0;i<element.length;i++){
+let items= element[i];
+items.style.backgroundColor ="#3baaf1";
+items.style.color ="white";
+}
+}
