25447f9df279a497fbbc73f8fbacf01813c4b671
sombir5 <sombirverma5@gmail.com>
2022-07-21 10:45:10 +0530

    tools js

---
+++ script/tools.js
@@ -0,0 +1,27 @@
+import navbar from "../component/navbar.js"
+document.getElementById('navbar').innerHTML=navbar();
+
+import footer from "../component/footer.js"
+document.querySelector('.footer').innerHTML=footer();
+
+let element=document.querySelectorAll(".points")
+console.log(element)
+element.forEach(e => {
+    e.addEventListener('click',function(){
+        changeColor(this)
+    });
+});
+function changeColor(element){
+    clear();
+    console.log(element)
+    element.style.backgroundColor ="white";
+    element.style.color ="black";
+}
+
+function clear(){
+for(let i=0;i<element.length;i++){
+let items= element[i];
+items.style.backgroundColor ="#3baaf1";
+items.style.color ="white";
+}
+}
